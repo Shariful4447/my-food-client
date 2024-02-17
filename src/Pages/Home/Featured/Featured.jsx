d9f5bab86ca6a1cc1c2300c1c8304f59d2b16f2b
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-17 16:12:44 +0100

    featured item section added

---
+++ src/Pages/Home/Featured/Featured.jsx
@@ -0,0 +1,20 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+
+const Featured = () => {
+    return (
+       
+        <section>
+            <SectionTitle
+                heading="featured item"
+                subHeading="check It Out"
+            >
+
+            </SectionTitle>
+
+        </section>
+       
+    );
+};
+
+export default Featured;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 
 
@@ -11,6 +12,7 @@ const Home = () => {
             <Category></Category>
             <About></About>
             <PopularMenu></PopularMenu>
+            <Featured></Featured>
         </div>
     );
 };
