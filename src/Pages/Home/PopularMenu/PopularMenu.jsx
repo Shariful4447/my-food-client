fc0ae12abbf57e00d80b758c36d63ed0e93cba1a
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-17 15:40:22 +0100

    popular menu section  added

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,18 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+
+const PopularMenu = () => {
+    return (
+        <section>
+            <SectionTitle
+                subHeading="Check it out"
+                heading="FROM OUR MENU"
+                
+            >
+
+            </SectionTitle>
+        </section>
+    );
+};
+
+export default PopularMenu;
