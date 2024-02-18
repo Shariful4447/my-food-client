10d821e20e0a9ae34a00a4feb5a33c52d34b8eb3
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-18 01:03:03 +0100

    update contact phone

--- src/Pages/Home/ContactPhone/ContactPhone.jsx
+++ src/Pages/Home/ContactPhone/ContactPhone.jsx
@@ -3,7 +3,7 @@
 const ContactPhone = () => {
     return (
         <div className="h-48 bg-black mt-10 mb-10">
-            <p className="text-3xl text-white text-center py-20">+49 1234567890</p>
+            <p className="text-3xl text-white text-center py-20">Call Us: +49 1234567890</p>
         </div>
     );
 };
