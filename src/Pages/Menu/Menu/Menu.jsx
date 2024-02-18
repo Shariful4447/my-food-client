365ee42b68990689c81d2003772314ab16c677f4
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-18 19:20:57 +0100

    parallox effect added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,9 @@
 import { Helmet} from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
+
 
 const Menu = () => {
     return (
@@ -15,7 +18,42 @@ const Menu = () => {
                 >
 
                 </Cover>
+                
             </div>
+            <PopularMenu></PopularMenu>
+
+            <div className='mb-10 bg-slate-500 bg-opacity-60'>
+                <Cover 
+                 img={menuImg}
+                 title="our menu"
+                >
+
+                </Cover>
+                
+            </div>
+            <PopularMenu></PopularMenu>
+
+            <div className='mb-10 bg-slate-500 bg-opacity-60'>
+                <Cover 
+                 img={menuImg}
+                 title="our menu"
+                >
+
+                </Cover>
+                
+            </div>
+            <PopularMenu></PopularMenu>
+            
+            <div className='mb-10 bg-slate-500 bg-opacity-60'>
+                <Cover 
+                 img={menuImg}
+                 title="our menu"
+                >
+
+                </Cover>
+                
+            </div>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,7 +1,13 @@
-
+import { Parallax } from 'react-parallax';
 
 const Cover = ({img, title}) => {
     return (
+        <Parallax
+        blur={{ min: -50, max: 50 }}
+        bgImage={img}
+        bgImageAlt="the dog"
+        strength={-200}
+    >
         <div className="hero h-[700px]" style={{backgroundImage: `url("${img}")`}}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
@@ -12,6 +18,8 @@ const Cover = ({img, title}) => {
                 </div>
             </div>
         </div>
+    </Parallax>
+        
     );
 };
 
