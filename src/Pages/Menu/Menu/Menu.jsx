0e2aab62553da1cf8fb0d41c74c91a98557ed220
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-18 19:03:57 +0100

    menu cover added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import { Helmet} from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
 
 const Menu = () => {
     return (
@@ -7,7 +8,14 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu </title>
             </Helmet>
-            <Cover></Cover>
+            <div className='mb-10 bg-slate-500 bg-opacity-60'>
+                <Cover 
+                 img={menuImg}
+                 title="our menu"
+                >
+
+                </Cover>
+            </div>
         </div>
     );
 };
