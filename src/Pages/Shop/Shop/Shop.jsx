bbef09a13ab10646662535df8c7c5dfec22fcf65
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-20 00:11:57 +0100

    create tab item of menu

--- src/Pages/Shop/Shop/Shop.jsx
+++ src/Pages/Shop/Shop/Shop.jsx
@@ -3,8 +3,10 @@ import Cover from "../../Shared/Cover/Cover";
 import shopImage from '../../../assets/shop/banner2.jpg'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { useState } from "react";
 
 const Shop = () => {
+    const [tabIndex, setTabIndex] =useState(0);
     return (
         <div>
             <Helmet>
@@ -13,16 +15,20 @@ const Shop = () => {
             <div className='mb-10 bg-slate-500 bg-opacity-60'>
                 <Cover img={shopImage} title="our Shop"></Cover>
             </div>
-            <Tabs defaultIndex={1} onSelect={(index) => console.log(index)}>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
                 <TabList>
                     <Tab>Salad</Tab>
                     <Tab>Pizza</Tab>
                     <Tab>Drinks</Tab>
                     <Tab>Soup</Tab>
-
+                    <Tab>Desserts</Tab>
                 </TabList>
                 <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                
             </Tabs>
         </div>
     );
