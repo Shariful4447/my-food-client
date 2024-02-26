a59dbbc9a7296a2cc1dacbaf640277eb4f037a5c
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-27 00:47:08 +0100

    total cart item show in shopping cart icon

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,11 @@ import {
 import Router from './Routes/Router';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Provider/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 
  
@@ -17,11 +22,14 @@ import AuthProvider from './Provider/AuthProvider';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <HelmetProvider>
-        <div className='max-w-screen-2xl mx-auto'>
-          <RouterProvider router={Router} />
-        </div>
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className='max-w-screen-2xl mx-auto'>
+            <RouterProvider router={Router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
+      
     </AuthProvider>
   </React.StrictMode>,
 )
