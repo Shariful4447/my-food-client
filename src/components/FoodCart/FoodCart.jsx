0b9d65f99aa4a5b95db6928945b786c96ffdcbb6
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-26 15:39:29 +0100

    shop item data with extract successfully

--- src/components/FoodCart/FoodCart.jsx
+++ src/components/FoodCart/FoodCart.jsx
@@ -1,7 +1,37 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 
 const FoodCart = ({item}) => {
     const {name, image, price, recipe}=item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const handleAddToCart = food=>{
+        console.log(food);
+        if(user && user?.email){
+            //send item to the database
+
+
+        }
+        else{
+            Swal.fire({
+                title: "You are not Logged In",
+                text: "Please Login to add Item to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, log In!"
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  //send to the login page
+                    navigate('/login')
+                }
+              });
+        }
+
+    }
     return (
         <div className="card w-[424px] bg-base-100 shadow-xl p-3">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -10,7 +40,7 @@ const FoodCart = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 
-                <div className="card-actions">
+                <div onClick={()=>handleAddToCart(item)} className="card-actions">
                 <button className="btn btn-outline bg-slate-200 border-0 border-orange-400 border-b-4">Buy Now</button>
                 </div>
             </div>
