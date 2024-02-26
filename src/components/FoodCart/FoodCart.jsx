00f716e1e01b5c34bac612c5ae5d446b7e25e136
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-26 22:03:40 +0100

    cart item added to database

--- src/components/FoodCart/FoodCart.jsx
+++ src/components/FoodCart/FoodCart.jsx
@@ -1,17 +1,38 @@
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
 const FoodCart = ({item}) => {
-    const {name, image, price, recipe}=item;
+    const {name, image, price, recipe, _id}=item;
     const { user } = useAuth();
     const navigate = useNavigate();
-    const handleAddToCart = food=>{
-        console.log(food);
+    const location = useLocation();
+    const handleAddToCart = ()=>{
+        
         if(user && user?.email){
             //send item to the database
-
+            const cartItem = {
+                menuId: _id,
+                email: user.email,
+                name,
+                image,
+                price
+            }
+            axios.post('http://localhost:8000/carts', cartItem)
+            .then(res =>{
+                console.log(res.data);
+                if(res.data.insertedId){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Item Added Successfully",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
+            })
 
         }
         else{
@@ -26,7 +47,7 @@ const FoodCart = ({item}) => {
               }).then((result) => {
                 if (result.isConfirmed) {
                   //send to the login page
-                    navigate('/login')
+                    navigate('/login', {state: {from: location}})
                 }
               });
         }
@@ -40,7 +61,7 @@ const FoodCart = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 
-                <div onClick={()=>handleAddToCart(item)} className="card-actions">
+                <div onClick={handleAddToCart} className="card-actions">
                 <button className="btn btn-outline bg-slate-200 border-0 border-orange-400 border-b-4">Buy Now</button>
                 </div>
             </div>
