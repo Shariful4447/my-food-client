ac7df727ba719d33133d40181b24e00d18756456
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-27 01:06:27 +0100

    automatically update cart count

--- src/components/FoodCart/FoodCart.jsx
+++ src/components/FoodCart/FoodCart.jsx
@@ -2,6 +2,7 @@ import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
 
 
 const FoodCart = ({item}) => {
@@ -10,6 +11,7 @@ const FoodCart = ({item}) => {
     const navigate = useNavigate();
     const location = useLocation();
     const axiosSecure = useAxiosSecure();
+    const [, refetch] = useCart();
 
     const handleAddToCart = ()=>{
         
@@ -34,6 +36,7 @@ const FoodCart = ({item}) => {
                         timer: 1500
                     });
                     // refetch cart to update the cart items count
+                    refetch();
                     
                 }
             })
