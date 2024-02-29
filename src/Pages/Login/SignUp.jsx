ee2b50cc449788529acbc2628980498bc71a3306
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-29 17:55:15 +0100

    sign up page design update

--- src/Pages/Login/SignUp.jsx
+++ src/Pages/Login/SignUp.jsx
@@ -2,14 +2,13 @@ import register from '../../assets/others/authentication2.png'
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import GoogleIcon from '@mui/icons-material/Google';
 import { Link, useNavigate} from 'react-router-dom';
 //import Swal from 'sweetalert2';
 import { useContext } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
 import Swal from "sweetalert2";
 import useAxiosPublic from '../../hooks/useAxiosPublic';
-import { Helmet } from "react-helmet-async";
+import signUp from '../../assets/others/authentication2.png'
 import { useForm } from "react-hook-form";
 
 const SignUp = () => {
@@ -95,11 +94,8 @@ const SignUp = () => {
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Sign up now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
+                <div className="hero-content flex-col lg:flex-row-reverse gap-10">
+                    
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             <div className="form-control">
@@ -147,6 +143,9 @@ const SignUp = () => {
                         </form>
                         <p className="px-6"><small>Already have an account <Link to="/login">Login</Link></small></p>
                         
+                    </div>
+                    <div className="text-center lg:text-left">
+                            <img className='h-[500px] w-[400px]' src={signUp} alt="" />
                     </div>
                 </div>
             </div>
