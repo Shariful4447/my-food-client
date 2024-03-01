b3a313c85844f894129bef2a53dfe7608ee06ce4
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-01 03:46:20 +0100

    make user and set role as admin done

--- src/Pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,19 +1,63 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { BsMicrosoftTeams } from "react-icons/bs";
+import Swal from "sweetalert2";
 
 
 
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: users =[] } = useQuery({
+    const { data: users =[], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async ()=>{
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+    const handleDeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+            
+            axiosSecure.delete(`/users/${user._id}`)
+            .then(res =>{
+                if(res.data.deletedCount > 0){
+                    refetch();
+                    Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                          });
+                }
+            })
+            }
+          });
+
+    }
+    const handleMakeAdmin = user =>{
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res =>{
+            console.log(res.data);
+            if(res.data.modifiedCount >0){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${user.name} is an admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
+    }
     return (
         <div className="m-5 p-5">
             <div>
@@ -34,18 +78,21 @@ const AllUsers = () => {
                         </thead>
                         <tbody>
                         {
-                            users.map(user=> <tr key={user._id}>
-                                <th>1</th>
+                            users.map((user, index)=> <tr key={user._id}>
+                                <th>{index +1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>
-                                    <button className="btn btn-circle btn-outline">
-                                        <BsMicrosoftTeams />
-                                    </button>
+                                    {
+                                        user.role === 'admin' ? 'Admin' : 
+                                        <button onClick={()=>handleMakeAdmin(user)} className="btn btn-circle btn-outline">
+                                            <BsMicrosoftTeams />
+                                        </button>
+                                    }
                                     
                                 </td>
                                 <td>
-                                    <button className="btn btn-circle btn-outline">
+                                    <button onClick={()=>handleDeleteUser(user)} className="btn btn-circle btn-outline">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                     </button>
                                 </td>
