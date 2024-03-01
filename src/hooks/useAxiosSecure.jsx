bea3af0b1db45ee751e4c6ba3af7fbe47fac3709
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-01 22:16:12 +0100

    set jwt token for user

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -6,6 +6,15 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function(config){
+        
+        const token = localStorage.getItem('access-token');
+        console.log('Request stopped by interceptor',token);
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    }, function(error){
+        return Promise.reject(error);
+    })
     return axiosSecure;
 };
 
