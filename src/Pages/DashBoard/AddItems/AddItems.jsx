55c0ad26b284bc6e4735845428b042c904f82c72
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-06 19:27:29 +0100

    form design added

--- src/Pages/DashBoard/AddItems/AddItems.jsx
+++ src/Pages/DashBoard/AddItems/AddItems.jsx
@@ -1,9 +1,31 @@
+import { useForm } from "react-hook-form";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 
 const AddItems = () => {
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (data) => console.log(data);
     return (
         <div>
-            <h2 className="text 4xl">Add New Items</h2>
+            
+            <SectionTitle 
+            heading='ADD AN ITEM'
+            subHeading='---Whats new?---'
+            ></SectionTitle>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input {...register("name")} />
+                    <select {...register("category")} className="select select-bordered w-full max-w-xs">
+                        <option disabled selected>Select a Category</option>
+                        <option value="salad">Salad</option>
+                        <option value="soup">Soup</option>
+                        <option value="deinks">Drinks</option>
+                        <option value="pizza">Pizza</option>
+                        <option value="desserts">Desserts</option>
+                    </select>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
