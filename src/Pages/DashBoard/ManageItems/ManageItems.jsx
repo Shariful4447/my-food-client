67fe9a46fca6332b7e43f347c479ac81e94148d1
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-08 00:13:57 +0100

    delete menu iten done successfully

--- src/Pages/DashBoard/ManageItems/ManageItems.jsx
+++ src/Pages/DashBoard/ManageItems/ManageItems.jsx
@@ -1,10 +1,46 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaRegEdit } from "react-icons/fa";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const ManageItems = () => {
-    const [menu] = useMenu();
+    const [menu, , refetch] = useMenu();
+    const axiosSecure = useAxiosSecure();
+    const handleDeleteItem = (item) => {
+
+        //Delete
+        Swal.fire({
+            title: "Are you sure? want to delete?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then( async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/menu/${item._id}`)
+                console.log(res.data);
+                if(res.data.deletedCount > 0) {
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${item.name} deleted successfully`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
+            //   Swal.fire({
+            //     title: "Deleted!",
+            //     text: "Your file has been deleted.",
+            //     icon: "success"
+            //   });
+            }
+          });
+    }
     return (
         <div>
             <SectionTitle
@@ -41,7 +77,7 @@ const ManageItems = () => {
                             <td>
                             <div className="flex items-center gap-3">
                                 <div className="avatar">
-                                <div className="mask mask-squircle w-12 h-12">
+                                <div className="mask mask-squircle w-16 h-16">
                                     <img src={item.image} alt="Avatar Tailwind CSS Component" />
                                 </div>
                                 </div>
@@ -54,12 +90,12 @@ const ManageItems = () => {
                             </td>
                             <td>{item.price}</td>
                             <th>
-                                <button className="btn btn-circle btn-outline">
+                                <button className="btn btn-circle bg-yellow-500 btn-outline">
                                 <FaRegEdit></FaRegEdit></button>
                             </th>
                             <th>
-                                <button className="btn btn-circle btn-outline">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                <button onClick={()=>handleDeleteItem(item)} className="btn btn-circle bg-red-500 btn-outline">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                 </button>
                             </th>
                             
