d340ca9469d4f6fa3d9b0d930c90e32d13ba0b6f
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-09 02:44:37 +0100

    payment with card done show transction id here

--- src/Pages/DashBoard/Payment/ChekoutForm.jsx
+++ src/Pages/DashBoard/Payment/ChekoutForm.jsx
@@ -1,12 +1,31 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
+
 
 
 
 const ChekoutForm = () => {
     const [error, setError] = useState('');
+    const[clientSecret, setClientSecret] = useState('');
+    const [transactionId, setTransactionId] = useState('');
+    const {user} = useAuth();
     const stripe = useStripe();
     const elements = useElements();
+    const axiosSecure = useAxiosSecure();
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+    useEffect(() => {
+        axiosSecure.post('/create-payment-intent', {price: totalPrice})
+        .then(res => {
+            console.log(res.data.clientSecret);
+            setClientSecret(res.data.clientSecret);
+        })
+    },[axiosSecure, totalPrice])
     const handleSubmit = async (event) => {
         event.preventDefault();
         if(!stripe || !elements){
@@ -29,6 +48,33 @@ const ChekoutForm = () => {
             console.log(paymentMethod);
             setError('');
         }
+        // confirm payment
+        const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email ||'anonymous',
+                    name: user?.displayName || 'anonymous'
+
+                }
+            }
+        })
+        if(confirmError){
+            console.log('confirm error');
+        }
+        else{
+            console.log('payment intent', paymentIntent);
+            if(paymentIntent.status === 'succeeded' ){
+                setTransactionId(paymentIntent.id);
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Payment Complete",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        }
 
     }
     return (
@@ -49,10 +95,11 @@ const ChekoutForm = () => {
                 },
                 }}
             />
-            <button className="btn btn-outline p-5 m-2" type="submit" disabled={!stripe}>
+            <button className="btn btn-outline p-5 m-2" type="submit" disabled={!stripe || !clientSecret}>
                 Pay
             </button>
             <p className="text-red-500 text-xl">{error}</p>
+            {transactionId && <p className="text-green-500"> your transction id : {transactionId}</p>}
         </form>
     );
 };
