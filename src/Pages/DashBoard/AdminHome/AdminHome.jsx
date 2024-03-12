a4a7ad7cfa3a06a5ef273772373391419dea6c8f
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-12 01:05:09 +0100

    error solve on dashboard stats

--- src/Pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/Pages/DashBoard/AdminHome/AdminHome.jsx
@@ -1,69 +1,66 @@
-import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from '@tanstack/react-query';
+import { FaBook, FaDollarSign, FaUsers } from 'react-icons/fa';
 
 
 const AdminHome = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const {data : stats} = useQuery({
-        queryKey:['admin-stats'],
-        queryFn: async() =>{
+    const { data: stats = {} } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
             const res = await axiosSecure.get('/admin-stats');
             return res.data;
         }
-    })
+    });
     return (
         <div>
                 <h2>Admin Home : {
                 user?.displayName ? user.displayName :'Welcome Back'
                 }</h2>
 
-                <div>
-                    <div className="stats shadow ml-48 ">
-    
-                        <div className="stat">
-                            <div className="stat-figure text-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-                            </div>
-                            <div className="stat-title">Revenue</div>
-                            <div className="stat-value text-primary">{stats.revenue}$</div>
-                            <div className="stat-desc">21% more than last month</div>
-                        </div>
-                        
-                        <div className="stat">
-                            <div className="stat-figure text-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                            </div>
-                            <div className="stat-title">Products</div>
-                            <div className="stat-value text-secondary">{stats.menuItems}</div>
-                            <div className="stat-desc">21% more than last month</div>
-                        </div>
-                        <div className="stat">
-                            <div className="stat-figure text-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                            </div>
-                            <div className="stat-title">Customers</div>
-                            <div className="stat-value text-secondary">{stats.users}</div>
-                            <div className="stat-desc">21% more than last month</div>
-                        </div>
-                        
-                        <div className="stat">
-                            <div className="stat-figure text-secondary">
-                            <div className="avatar online">
-                                <div className="w-16 rounded-full">
-                                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                                </div>
-                            </div>
-                            </div>
-                            <div className="stat-value">{stats.orders}</div>
-                            <div className="stat-title">Orders</div>
-                            <div className="stat-desc text-secondary">31 tasks remaining</div>
-                        </div>
-    
+            <div className="stats shadow">
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaDollarSign className='text-3xl'></FaDollarSign>
                     </div>
+                    <div className="stat-title">Revenue</div>
+                    <div className="stat-value">${stats.revenue}</div>
+                    <div className="stat-desc">Jan 1st - Feb 1st</div>
                 </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaUsers className='text-3xl'></FaUsers>
+                    </div>
+                    <div className="stat-title">Users</div>
+                    <div className="stat-value">{stats.users}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaBook className='text-3xl'></FaBook>
+                    </div>
+                    <div className="stat-title">Menu Items</div>
+                    <div className="stat-value">{stats.menuItems}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                    </div>
+                    <div className="stat-title">Orders</div>
+                    <div className="stat-value">{stats.orders}</div>
+                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+
+            </div>
         </div>
     );
 };
