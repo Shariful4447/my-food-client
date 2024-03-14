fa45918589e67d4115597681ee141c6a359336e1
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-14 01:25:51 +0100

    pie and bar chart complete

--- src/Pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/Pages/DashBoard/AdminHome/AdminHome.jsx
@@ -3,22 +3,14 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from '@tanstack/react-query';
 import { FaBook, FaDollarSign, FaUsers } from 'react-icons/fa';
 
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, PieChart, Pie, Legend } from 'recharts';
 
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const getPath = (x, y, width, height) => {
-    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
-    ${x + width / 2}, ${y}
-    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
-    Z`;
-  };
-  
-  const TriangleBar = (props) => {
-    const { fill, x, y, width, height } = props;
-  
-    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-  };
+
+
+
 const AdminHome = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
@@ -38,6 +30,36 @@ const AdminHome = () => {
         }
 
 
+    })
+    // custom bar chart
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+      };
+      
+      const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+      
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+      };
+
+    //   custom pie chart 
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+      const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+      const x = cx + radius * Math.cos(-midAngle * RADIAN);
+      const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    
+      return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+          {`${(percent * 100).toFixed(0)}%`}
+        </text>
+      );
+    };
+    const pieChartData= chartData.map(data =>{
+        return {name: data.category, value:data.revenue}
     })
     return (
         <div>
@@ -109,7 +131,23 @@ const AdminHome = () => {
                     </BarChart>
                 </div>
                 <div className="w-1/2">
-
+                <PieChart width={600} height={600}>
+                    <Pie
+                        data={pieChartData}
+                        cx="60%"
+                        cy="60%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {pieChartData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Legend></Legend>
+                </PieChart>
                 </div>
             </div>
 
