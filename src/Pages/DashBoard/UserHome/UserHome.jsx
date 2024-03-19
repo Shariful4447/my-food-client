42b9ab42ebf885670ecad119ad8db2f3feee14ee
Shariful4447 <shariful.islam4447@gmail.com>
2024-03-20 00:49:38 +0100

    pic added in the user dashboard

--- src/Pages/DashBoard/UserHome/UserHome.jsx
+++ src/Pages/DashBoard/UserHome/UserHome.jsx
@@ -3,13 +3,15 @@ import useAuth from "../../../hooks/useAuth";
 
 const UserHome = () => {
     const {user} = useAuth();
+    console.log(user);
     return (
-        <div>
+        <div className="flex flex-1 justify-between bg-blue-400 border-2 rounded-xl p-5">
             <h2>User Home 
                 {
                     user?.displayName ? user.displayName : 'Welcome'
                 }
             </h2>
+            <img className="border rounded-full h-12" src={user.photoURL} alt="" />
         </div>
     );
 };
